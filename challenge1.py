d025be02b0379d3f71d4f0ae541a54465503c968
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-29 20:39:43 -0400

    Completes exercise #2 of challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -1,8 +1,17 @@
-# Create a list of 10 random numbers between -25 and 25
+# 1. Create a list of 10 random numbers between -25 and 25
 
 import random
 
 random_list = []
 
 for x in range(10):
-    print(random.randint(-25, 25))
+    random_list.append(random.randint(-25, 25))
+
+print(random_list)
+
+# 2. Use the list from question 1 and, utilizing a list comprehension, 
+#    create a new list that only contains the negative numbers.
+
+y = [i for i in random_list if i < 0]
+
+print(y)
