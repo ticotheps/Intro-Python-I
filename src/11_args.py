e0c4f594e7f91631357f42e597b3bba223ec6e6c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-29 23:13:31 -0400

    Completes half of 11_args.py challenge

--- src/11_args.py
+++ src/11_args.py
@@ -4,14 +4,21 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+def f1(arg1, arg2):
+    return (int(arg1) + int(arg2))
 
-print(f1(1, 2))
+print(f1(4, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+def f2(*argv):
+    sum = 0
+    for i in argv:
+        sum += i
+    return sum
+    
+print(f2(13,24,32))
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -20,14 +27,26 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
-# What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+# What thing do you have to add to make this work? AN ASTERISK!
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+def f3(*argv):
+    if len(argv) == 0:
+        returned_num =0
+        return returned_num
+    elif len(argv) == 1:
+        returned_num = 0
+        returned_num = argv[0] + 1
+        return returned_num
+    elif len(argv) == 2:
+        returned_num =0
+        returned_num = argv[0] + argv[1]
+        return returned_num
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -46,18 +65,18 @@ print(f3(8))     # Should print 9
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-f4(a=12, b=30)
+# f4(a=12, b=30)
 
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+# f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
-    "monster": "goblin",
-    "hp": 3
-}
+# d = {
+#     "monster": "goblin",
+#     "hp": 3
+# }
 
-# What thing do you have to add to make this work?
-f4(d)
+# # What thing do you have to add to make this work?
+# f4(d)
