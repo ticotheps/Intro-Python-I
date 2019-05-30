ff589b1f95aa0ab169cae8153da158c9f9358cd2
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-29 20:45:15 -0400

    Completes exercise #3 of challenge1.py

--- challenge1.py
+++ challenge1.py
@@ -9,9 +9,21 @@ for x in range(10):
 
 print(random_list)
 
+
 # 2. Use the list from question 1 and, utilizing a list comprehension, 
 #    create a new list that only contains the negative numbers.
 
 y = [i for i in random_list if i < 0]
 
 print(y)
+
+
+# 3. Create a tuples with 3 of your favorite foods, loop through the 
+#    tuple and print out each item.
+#    tuples = read-only type of list in python
+
+z = ("spaghetti", "cheese burgers", "pizza")
+
+for i in z:
+    print(i)
+
