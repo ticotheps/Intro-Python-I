991bd1c861ed8b9f7b805d42339789a5c45648e6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-05-29 20:27:08 -0400

    Adds test.py and adds challenge1.py files

---
+++ test.py
@@ -0,0 +1,18 @@
+print("Here is a test")
+
+int_list = [0, 1, 2, 3, 4, 5]
+
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+str_list = [str(i) for i in range(500-1000) if i % 2 == 0]
+
+# list_comprehension [  FORMATTING   COLLECTION   CONDITION  ]
+
+# This is a comment
+
+'''
+This is a multiline comment
+'''
+
+# in python, "null" is "None"
