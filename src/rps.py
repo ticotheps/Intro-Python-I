b36605444f44d39b89abd35af25f6048d527b6fe
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-03 13:45:29 -0500

    Adds a rps.py file during lecture

---
+++ src/rps.py
@@ -0,0 +1,11 @@
+# Create a REPL loop to process commands
+while True:
+  
+
+# REPL should accept 'r', 'p', 's' commands
+# 'q' to quit
+
+# When a player types r/p/s, CPU will pick a random choice
+
+# Print out the score and loop
+
